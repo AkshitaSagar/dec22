a2e4d66936094e3689abfc5c939ec8d9cfd904ca
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-30 23:29:44 +0530

    Add files via upload

---
+++ 22dec21.js
@@ -0,0 +1,17 @@
+//Assignment: create an array using .push 
+
+/*var arr = ['one', 'two', 'three']; 
+   arr.push('ten');
+     alert(arr);*/
+
+
+
+// Take three numbers and find max. (Using arrays) 
+
+numbers_array = [1,2,3,10,78]
+max_value = Math.max.apply(null,numbers_array)
+console.log(max_value)
+
+
+
+
